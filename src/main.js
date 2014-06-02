bb945b93a163da67626438f0c1cc82c1e5b07cfa
git <git@192.168.0.2>
2014-06-02 10:52:39 -0700

    textures added

--- src/main.js
+++ src/main.js
@@ -3,72 +3,98 @@ if (!Detector.webgl)
 
 var fluid;
 
-var camera, controls, scene, renderer;
+var camera, controls, scene, renderer, groundPlane;
+var source = new THREE.Vector3(0, 0, 0);
 
-var mesh, smokeParticles, smoke;
+var mesh, smokeParticles;
 
-var PARTICLE_COUNT = 10000;
-var PARTICLE_SIZE = 50;
+var PARTICLE_COUNT = 3000;
+var PARTICLE_SIZE = 500;
+var PARTICLE_OPACITY = 0.02;
+
 init();
 animate();
 
 function init() {
-
 	// fluid
 	fluid = new CS274C.Fluid();
 
 	// camera
 	camera = new THREE.PerspectiveCamera(27, window.innerWidth / window.innerHeight, 5, 3500);
-	camera.position.z = 2750;
+	camera.position.z = 1600;
+	camera.position.y = 1500;
 	controls = new THREE.OrbitControls(camera);
 	controls.addEventListener('change', render);
 
 	// scene
 	scene = new THREE.Scene();
 
+	// TODO: shader?
+
+	// environment
+
+	// surrounding sky
+	var skyGeometry = new THREE.SphereGeometry(1200, 50, 40);
+	var skyMaterial = new THREE.MeshBasicMaterial({
+		map : THREE.ImageUtils.loadTexture('../images/sky.png'),
+	});
+
+	var skyBox = new THREE.Mesh(skyGeometry, skyMaterial);
+	skyBox.scale.set(-1, 1, 1);
+	skyBox.eulerOrder = 'XZY';
+	skyBox.renderDepth = 1000.0;
+	scene.add(skyBox);
+
+	// ground
+	var groundGeometry = new THREE.PlaneGeometry(2000, 2000);
+	var planeMeterial = new THREE.MeshBasicMaterial({
+		map : THREE.ImageUtils.loadTexture('../images/ground.jpg'),
+	});
+
+	groundPlane = new THREE.Mesh(groundGeometry, planeMeterial);
+	groundPlane.rotation.x = -90 * Math.PI / 180  ;
+	scene.add(groundPlane);
+
 	// smoke
 	smokeParticles = new THREE.Geometry;
 	for (var i = 0; i < PARTICLE_COUNT; i++) {
-		var particle = new THREE.Vector3(0, 0, 0);
+		var particle = new THREE.Vector3(source.x, source.y, source.z);
 		smokeParticles.vertices.push(particle);
 	}
 
 	var smokeMaterial = new THREE.ParticleBasicMaterial({
-		// map: THREE.ImageUtils.loadTexture('../images/particle.png'),
+		map : THREE.ImageUtils.loadTexture('../images/smoke.png'),
 		transparent : true,
-		blending : THREE.AdditiveBlending,
+		opacity: PARTICLE_OPACITY,
+		blending : THREE.NormalBlending,
+		depthTest : true,
 		size : PARTICLE_SIZE,
-		color : 0x111111
 	});
 
-	smoke = new THREE.ParticleSystem(smokeParticles, smokeMaterial);
+	var smoke = new THREE.ParticleSystem(smokeParticles, smokeMaterial);
 	smoke.sortParticles = true;
 	scene.add(smoke);
+
+	// lights
+	// TODO:
 
 	renderer = new THREE.WebGLRenderer({
 		antialias : false
 	});
-	renderer.setClearColor(0x000000, 1);
+	renderer.setClearColor(0x00ff00, 1);
 	renderer.setSize(window.innerWidth, window.innerHeight);
 
 	document.body.appendChild(renderer.domElement);
 	window.addEventListener('resize', onWindowResize, false);
-
 }
 
 function onWindowResize() {
-
 	windowHalfX = window.innerWidth / 2;
 	windowHalfY = window.innerHeight / 2;
-
 	camera.aspect = window.innerWidth / window.innerHeight;
 	camera.updateProjectionMatrix();
-
 	renderer.setSize(window.innerWidth, window.innerHeight);
-
 }
-
-//
 
 function animate() {
 	requestAnimationFrame(animate);
@@ -86,8 +112,8 @@ function animateSmoke() {
 	for (var i = 0; i < points.length; i++) {
 		if (points[i].y >= window.innerHeight) {
 			points[i].y = 0;
-			points[i].x = 0;
-			points[i].z = 0;
+			points[i].x = source.x;
+			points[i].z = source.z;
 		}
 	}
 }
